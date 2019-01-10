94f83022dd4356a6e3ece088c9f3e2d773a750eb
kevreez <41712873+kevreez@users.noreply.github.com>
2019-01-10 16:07:41 -0500

    Add files via upload

---
+++ Exercise 1A/CircumferenceandArea/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircumferenceandArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a radius value and the" +
+                 "circumference and area of a circle will be calculated");
+            string inputValue = Console.ReadLine();
+            double radiusValue = double.Parse(inputValue);
+            double circumference = 2.0 * Math.PI * radiusValue;
+            double area = Math.PI * Math.Pow(radiusValue, 2);
+
+            Console.WriteLine("circumference: " + circumference);
+            Console.WriteLine("area: " + area);
+            Console.ReadLine();
+        }
+    }
+}
